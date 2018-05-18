54beb091110b2c3a0d02aa546811c9fce83888f9
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-05-18 20:43:57 +0530

    master: stock-service : Eureka client config

--- stock-service/src/main/java/com/lakshman/stock/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/lakshman/stock/stockservice/StockServiceApplication.java
@@ -2,8 +2,9 @@ package com.lakshman.stock.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class StockServiceApplication {
 
