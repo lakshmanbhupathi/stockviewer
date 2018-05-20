00b57ffa03ee1cc8112fbe3e1e36fe218bd7beb8
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-05-20 17:05:57 +0530

    master: zuul-gataway : initial zuul-gataway code check in

---
+++ zuul-gateway/src/main/java/com/lakshman/stock/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lakshman.stock.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
